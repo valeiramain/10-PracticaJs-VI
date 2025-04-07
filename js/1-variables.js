c9bba59dfee13b725621b5d3b221100059f6a211
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-07 11:34:19 -0300

    se realizo declaracion y uso de variables

---
+++ js/1-variables.js
@@ -0,0 +1,27 @@
+// Comentario de una linea
+console.log("Mensaje de Prueba")
+
+document.writeln("<p>Feliz cumple Vale 🎈 </p>")
+document.writeln("Feliz cumple Vale 🎈")
+
+// VARIABLES (let - const. no usar var)
+let anioNuevo;
+let anio = 2025;
+
+document.writeln('<br>Año actual: '+anio)
+document.writeln('<br>Año actual: ',anio)
+
+anio = 2024
+
+const url = "http://127.0.0.1:5500/"
+document.writeln('<br>El link de liveServer es: '+url)
+
+// una variable definida como CONST no puede cambiar de valor
+
+// Sumar de 2 numeros - definir con constante
+let numero1, numero2, resultado
+
+numero1 = 10
+numero2 = 12
+resultado = numero1 + numero2
+document.writeln("<br>La suma de los dos numeros ingresados es: "+resultado)
