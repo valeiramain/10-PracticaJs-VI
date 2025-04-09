b09fb49a6c5b6d4e797b809fab2260078338044f
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-09 09:53:44 -0300

    agregamos prompt para que el usuario ingrese los numeros a sumar

--- js/1-variables.js
+++ js/1-variables.js
@@ -1,27 +1,29 @@
 // Comentario de una linea
-console.log("Mensaje de Prueba")
+console.log("Mensaje de Prueba");
 
-document.writeln("<p>Feliz cumple Vale 🎈 </p>")
-document.writeln("Feliz cumple Vale 🎈")
+document.writeln("<p>Feliz cumple Vale 🎈 </p>");
+document.writeln("Feliz cumple Vale 🎈");
 
 // VARIABLES (let - const. no usar var)
 let anioNuevo;
 let anio = 2025;
 
-document.writeln('<br>Año actual: '+anio)
-document.writeln('<br>Año actual: ',anio)
+document.writeln("<br>Año actual: " + anio);
+document.writeln("<br>Año actual: ", anio);
 
-anio = 2024
+anio = 2024;
 
-const url = "http://127.0.0.1:5500/"
-document.writeln('<br>El link de liveServer es: '+url)
+const url = "http://127.0.0.1:5500/";
+document.writeln("<br>El link de liveServer es: " + url);
 
 // una variable definida como CONST no puede cambiar de valor
 
 // Sumar de 2 numeros - definir con constante
 // let numero1, numero2, resultado
+const numero1 = parseInt(prompt('Ingrese primer número: '))
+const numero2 = parseInt(prompt('Ingrese segundo número: '))
 
-const numero1 = 10
-const numero2 = 12
-const resultado = numero1 + numero2
-document.writeln("<br>La suma de los dos numeros ingresados es: "+resultado)
+// const numero1 = 10
+// const numero2 = 12
+const resultado = numero1 + numero2;
+document.writeln("<br>La suma de los dos numeros ingresados es: " + resultado);
