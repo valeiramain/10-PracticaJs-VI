db4568fdb12b3e3c1b410ec0d6174975740a4171
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-09 10:34:00 -0300

    agregamos sintaxis de condicionales simples y dobles

---
+++ js/2-condicionales.js
@@ -0,0 +1,24 @@
+//sintaxis de un codicional
+//estructura de decision
+
+/*
+condicional simple
+if(condicion logica){
+    codigo a ejecutar
+}
+
+condicional doble
+if(condicion logica){
+    codigo a ejecutar
+}else{
+    codigo a ejecutar si no se cumple la condicion
+}
+
+if(condicion logica){
+    codigo a ejecutar
+}elseif (condicion logica2){
+    codigo a ejecutar
+}
+}
+
+*/
