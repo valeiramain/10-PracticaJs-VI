2e6177812f6e18efce8e102b91d2f2cd01aaa66f
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-14 10:24:23 -0300

    agregamos validacion de minusculas

--- js/2-tp1-ejercicio9.js
+++ js/2-tp1-ejercicio9.js
@@ -3,10 +3,12 @@
 
 //restringir la cantidad de caracteres que ingresa el usuario
 
-const frase = prompt("Ingrese una frase de hasta 5 caracteres: ");
+const frase = prompt("Ingrese una frase de hasta 5 caracteres: ").toLowerCase();
 
 console.log(frase)
 console.log(frase.length)
+console.log(frase.toUpperCase())
+console.log(frase.toLowerCase())
 console.log(frase.charAt(0))
 
 if (frase.length <= 5) {
@@ -14,16 +16,16 @@ if (frase.length <= 5) {
         document.writeln(frase.charAt(0))
     }
     if (frase.charAt(1) === "a" || frase.charAt(1) === "e" || frase.charAt(1) === "i" || frase.charAt(1) === "o" || frase.charAt(1) === "u") {
-        document.writeln(frase.charAt(0))
+        document.writeln(frase.charAt(1))
     }
     if (frase.charAt(2) === "a" || frase.charAt(2) === "e" || frase.charAt(2) === "i" || frase.charAt(2) === "o" || frase.charAt(2) === "u") {
-        document.writeln(frase.charAt(0))
+        document.writeln(frase.charAt(2))
     }
     if (frase.charAt(3) === "a" || frase.charAt(3) === "e" || frase.charAt(3) === "i" || frase.charAt(3) === "o" || frase.charAt(3) === "u") {
-        document.writeln(frase.charAt(0))
+        document.writeln(frase.charAt(3))
     }
     if (frase.charAt(4) === "a" || frase.charAt(4) === "e" || frase.charAt(4) === "i" || frase.charAt(4) === "o" || frase.charAt(4) === "u") {
-        document.writeln(frase.charAt(0))
+        document.writeln(frase.charAt(4))
     }
 } else {
     alert("Ingresaste una cadena inválida")
