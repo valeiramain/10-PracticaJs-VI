f66fc0ce458a42239e6965bff1a718c5c08cbddd
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-14 11:10:48 -0300

    Se completo codigo cado 2 del ejemlpo de cajero automatico

---
+++ js/3-switch.js
@@ -0,0 +1,36 @@
+// crear un programa al estilo de un cajero automatico con las sgtes opciones:
+// 1- cansultar saldo 
+// 2-ingresar dinero 
+// 3-extraer dinero
+
+// switch(opcion){
+//     case 1:
+//         codigo para la opcion 1
+//     case 2:
+//         codigo para la opcion 2
+//     case 3:
+//         codigo para la opcion 3
+//     default:
+//         codigo si la opcion no coincide con ninguno de los casos programados
+// }
+
+
+
+const opcion = parseInt(prompt("Ingrese una opción (1-Consultar el saldo, 2-Ingresar dinero, 3-Extraer dinero): "));
+let saldo = 10000;
+
+switch(opcion){
+    case 1: 
+        document.writeln("Su saldo es: $"+saldo);
+        break;
+    case 2:
+        const deposito = Number(prompt("Ingresa el monto a depositar: $"));
+        console.log(deposito);
+        saldo = saldo + deposito;
+        // saldo += deposito;
+        document.writeln("</br>Depositaste $"+deposito+", tu saldo actual es $"+saldo);
+        document.writeln(`<br>Depositaste ${deposito}, tu saldo es ${saldo}`)
+    case 3:
+        
+    default:
+}
