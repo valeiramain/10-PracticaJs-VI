c9f4dda58eccba84196d9a4c606a94605664dded
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-16 09:33:47 -0300

    creo rama bucles

---
+++ js/4-bucles.js
@@ -0,0 +1,15 @@
+//bucles while - do while - for
+
+/*
+while = miestras
+while(condicion logica){
+    todas las lineas de codigo que quiero repetir muchas veces
+    agregar algun codigo para que la condicion logica se deje de cumplir en algun momento
+    }
+*/
+
+let renglon = 1;
+while (renglon <= 50) {
+    document.writeln("<p>Renglón Número </p>")
+    renglon = renglon++;
+}
