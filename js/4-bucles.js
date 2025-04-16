9edb4bbfed5e6ac0acb190df53022f52c8af37dc
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-16 09:52:59 -0300

    agregamos la plantilla con las estructuras while, do while y for

--- js/4-bucles.js
+++ js/4-bucles.js
@@ -8,8 +8,31 @@ while(condicion logica){
     }
 */
 
-let renglon = 1;
-while (renglon <= 50) {
-    document.writeln("<p>Renglón Número </p>")
-    renglon = renglon++;
+let renglon = 1
+while(renglon <= 50){
+    document.writeln("</br> Renglón Número"+renglon)
+    renglon++
+}
+
+//do while (hacer mientras)
+/*
+do{
+    todas las lineas de codigo que quiero repetir muchas veces
+    agregar algun codigo para que la condicion logica se deje de cumplir en algun momento
+}while (condicion logica)
+*/
+
+let linea = 1
+do{
+    document.writeln("</br>linea numero ="+linea)
+    linea++
+}while(linea <=50)
+
+//for
+/*
+for(let variable=0;variable<=50;variable++)
+*/
+
+for(let i=10;i>0;i--){
+    document.writeln("</br>contador ="+i)
 }
